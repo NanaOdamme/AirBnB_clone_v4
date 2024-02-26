e64f49efbcdc5811161df0a395ba328e525aba35
NanaOdamme <eronastyles@gmail.com>
2024-02-26 10:43:14 +0000

    updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,11 +2,23 @@
 """
 This module contains the index of application
 """
+from models import storage
 from api.v1.views import app_views
-from flask import jsonify
+from flask import Flask, jsonify
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """Returns the status of the API"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def count():
+    """Retrieve the number of each object type"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")}) 
