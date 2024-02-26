01f1a492c0def154a2a0905d08ee10e827dfce2a
NanaOdamme <eronastyles@gmail.com>
2024-02-26 13:21:04 +0000

    updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,9 +6,17 @@ from flask import Flask, jsonify
 from os import getenv
 from api.v1.views import app_views
 from models import storage
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def close_db(obj):
+    """ calls methods close() """
+    storage.close()
 
 
 @app.errorhandler(404)
@@ -22,7 +30,9 @@ def teardown_appcontext(exception):
     """Teardown method to close the storage"""
     storage.close()
 
+
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', 5000))
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = (getenv('HBNB_API_PORT', default=5000))
+
     app.run(host=host, port=port, threaded=True)
